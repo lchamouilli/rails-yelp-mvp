fb5dac556e826e7238bd10fea9c9c37d1ecad253
lionel chamouilli <lionel.chamouilli@gmail.com>
2022-03-23 16:06:48 +0100

    Second Exercice - Yelp MVP - Finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+oai = { name: "Oai", address: "40 Rue Sainte, 13001 Marseille", category: "french" }
+splendido = { name: "Splendido", address: "16-18 Rue de la République, 13001 Marseille", category: "italian" }
+le_yen = { name: "Le Yen", address: "60 Av. du Prado, 13006 Marseille", category: "japanese" }
+top_bun = { name: "Top Bun", address: "5 Rue Henri Barbusse", category: "belgian" }
+maison_ravioli = { name: "La maison du ravioli", address: "14 Rue d'Italie, 13006 Marseille", category: "chinese" }
+
+[oai, splendido, le_yen, top_bun, maison_ravioli].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
