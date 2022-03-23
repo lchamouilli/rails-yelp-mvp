8be6a119708d5ee1eed74ca07e7b9c3b0eac8f7d
lionel chamouilli <lionel.chamouilli@gmail.com>
2022-03-23 13:03:56 +0100

    Models Restaurant and Review Ok

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  # before_action :set_restaurant, only: [:show, :create]
+
+  # def index
+  #   @restaurants = Restaurant.all
+  # end
+
+  # def show
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # end
+
+  # def create
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  # private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+end
